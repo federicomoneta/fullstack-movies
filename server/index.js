fc388975285bcc8daaf05cef29e051da54398753
fedemos <fedemos123@gmail.com>
2021-06-18 13:12:14 -0300

    proyecto agregado + README.md

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Admin2020%',
+    database: 'CRUDDataBase',
+    port: '3306'
+})
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post('/api/insert', (req, res) => {
+
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?);";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(result);
+    })
+});
+
+app.get('/api/get', (req,res) => {
+    const sqlSelect = "SELECT * FROM movie_reviews;";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+});
+
+
+app.listen(3001, () => {
+    console.log("corriendo en el puerto 3001");
+});
