e1c545979f6d47c7b16beb2f6a49464fa3a373bc
martingetzel <martingetzel@gmail.com>
2021-06-23 16:26:25 -0300

    añadiendo inicio.sql

---
+++ inicio.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE CRUDDataBase;
+USE CRUDDataBase;
+CREATE TABLE movie_reviews(
+	id	integer auto_increment,
+	movieName	varchar(100),
+	movieReview	text,
+	PRIMARY KEY (id));
+	
+INSERT INTO movie_reviews (movieName, movieReview) VALUES ("pelicula1", "buena");
